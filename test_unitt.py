eb88a16a7ce0e4bb0dc5db5b6ee720fda5caa4b8
Maesloop <skarp1xel@gmail.com>
2025-04-03 14:48:22 +0200

    finalV1

--- test_unitt.py
+++ test_unitt.py
@@ -19,6 +19,21 @@ class TestShoppingCart(unittest.TestCase):
     def test_update_price(self):
         self.cart.add_item("jablko", 30, 5)
         self.assertTrue(self.cart.update_item_price("jablko",20))
+    def test_clear_cart(self):
+        self.cart.add_item("jablko", 20,5)
+        self.cart.clear_cart()
+        self.assertEqual(self.cart.clear_cart(), True)
+    def test_get_item_counts(self):
+        self.cart.add_item("jablko", 20,2)
+        self.cart.add_item("hruska", 20,5)
+        self.assertEqual(self.cart.get_item_count(),7)
+        print(self.cart.get_item_count())
+    def test_cart_contents(self):
+        self.cart.add_item("jablko", 20,2)
+        self.cart.add_item("hruska", 20,3)
+        print(self.cart.get_cart_contents())
+        expected = ['jablko: 2 ks × 20 Kč', 'hruska: 3 ks × 20 Kč']
+        self.assertEqual(self.cart.get_cart_contents(),expected)
     
     
 
