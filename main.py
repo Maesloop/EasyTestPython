4abc72a58d1a270b77312ef2a70ca9353313704a
Maesloop <skarp1xel@gmail.com>
2025-04-03 10:34:56 +0200

    :UpdateV3

--- main.py
+++ main.py
@@ -6,10 +6,9 @@ cart.add_item("rohlik",100,2)
 cart.remove_item("rohlik",2)
 cart.apply_discount("SLEVA10")
 cart.get_total()
-print(cart.update_item_price("caj",50))
+print(cart.remove_item("maslo"))
 print(cart.get_total())
 print(cart.get_item_count())
 print(cart.get_cart_contents())
-cart.clear_cart()
-print(cart.get_cart_contents())
+
 
